fdd9d06f1e9a211547560be46b7d9b249cca06a0
Kyra <10883023@uvu.edu>
2022-04-14 22:22:28 -0600

    loops

--- Retro FPS Clone/Assets/Scripts/Assinments/Loops.cs
+++ Retro FPS Clone/Assets/Scripts/Assinments/Loops.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class Loops : MonoBehaviour
 {
         //public int numEwoks = 0;
+        public int numPeeps = 0;
+        public int numFlowers = 0;
+        public int numCars = 0;
+        public int numGnomes = 0;
         public int ducksInPond = 0;
+        public int trainsRunning = 0;
+        public int leavesOnTheTree = 0;
+        public int gallonsOfJuice = 0;
+        public int milesDriven = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,19 +23,52 @@ public class Loops : MonoBehaviour
       {
             Debug.Log("There are " + i + " Ewoks on a log!");
       }  
-      */
-
+      for(int i = 30; i >= numPeeps; i-- )
+      {
+            Debug.Log("There are " + i + " peeps in my tummy!");
+      }  
+       for(int i = 80; i >= numFlowers; i-- )
+      {
+            Debug.Log("There are " + i + " blooming in the garden!");
+      }  
+       for(int i = 43; i >= numCars; i-- )
+      {
+            Debug.Log("There are " + i + " Cars in the junkyard!");
+      }  
+       for(int i = 23; i >= numGnomes; i-- )
+      {
+            Debug.Log("There are " + i + " gnomes protecting the garden!");
+      }  
+        */
         while(ducksInPond < 99)
         {
             Debug.Log(ducksInPond);
             Debug.Log("All of the ducks have flown south for the winter.");
             ducksInPond ++;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+         while(trainsRunning < 54)
+        {
+            Debug.Log(trainsRunning);
+            Debug.Log("All of the trains have stopped running for the day.");
+            trainsRunning ++;
+        }
+           while(leavesOnTheTree < 125)
+        {
+            Debug.Log(leavesOnTheTree);
+            Debug.Log("All of the leaves have fallen off the tree.");
+            leavesOnTheTree ++;
+        }
+        while(gallonsOfJuice < 31)
+        {
+            Debug.Log(gallonsOfJuice);
+            Debug.Log("They drank all the juice.");
+            gallonsOfJuice ++;
+        }
+        while(milesDriven < 324)
+        {
+            Debug.Log(milesDriven);
+            Debug.Log("You have arrived!");
+            milesDriven ++;
+        }
+   
 }
--- Retro FPS Clone/Assets/Scripts/PlayerController.cs
+++ Retro FPS Clone/Assets/Scripts/PlayerController.cs
@@ -11,9 +11,9 @@ public class PlayerController : MonoBehaviour
     private Vector2 mouseInput;
 
     public float mouseSensitivity = 1.0f;
-    public Camera videoCam; 
-    public GameObject dangerdotsImpact;
-    public int currentDots; 
+    public Camera viewCam; 
+    public GameObject DangerdotImpact;
+    public int currentDangerdots; 
 
     private void Awake()
     {
@@ -32,6 +32,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         moveInput = new Vector3(Input.GetAxis("Horizontal"), 0,Input.GetAxis("Vertical)")); //Getting Imput
         Vector3 moveH = transform.right * moveInput.x; 
         Vector3 moveV = transform.forward * moveInput.z;
@@ -39,23 +40,24 @@ public class PlayerController : MonoBehaviour
 
         mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * mouseSensitivity;
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - mouseInput.x, transform.rotation.eulerAngles.z);
-    if(Input.GetMouseButtonDown(0))
+       
+        if(moveInput.GetMouseButtonDown(0))
         {
-        if(currentDots > 0)
+            if(currentDangerdots > 0)
             {
             Ray ray = viewCam.ViewportPointToRay(new Vector3(0.5f,0.5f,0f));
             RaycastHit hit;
 
-                if( Physics.Raycast(ray. out hit))
-                {
-                    Debug.Log("I'm looking at " + hit.transform.name);
-                    Instantiate(DangerdotImpact, hit.point, transform.rotation);
-                }
-                else
-                {
-                    Debug.Log("I'm not looking at anything!");
-                }
-                currentDots --;
+            if( Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("I'm looking at " + hit.transform.name);
+                Instantiate(DangerdotImpact, hit.point, transform.rotation);
+            }
+            else
+            {
+                Debug.Log("I'm not looking at anything!");
+            }
+                    currentDangerdots --;
             }
         }
     }     
